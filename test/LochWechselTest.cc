0443873bff7c1e8ec86736293ad61831089e1482
Alex Bepple <alex@bepple.de>
2013-07-01 22:08:39 +0200

    ~ sprechende Assertion

--- test/LochWechselTest.cc
+++ test/LochWechselTest.cc
@@ -3,10 +3,17 @@
 #include "scorecard.h"
 #include "lochwechsel.h"
 
+::testing::AssertionResult StrContains(char* haystack, char* needle) {
+	char* pos = strstr(haystack, needle);
+	if (pos > haystack) {
+		return ::testing::AssertionSuccess();
+	}
+	return ::testing::AssertionFailure() << "'" << needle << "' not in '" << haystack << "'";
+}
+
 TEST(Lochwechsel, gibtNeuesLochAus) {
 	struct scorecard sc = { 0, 0 };
 	char* ausgabe = naechstesLoch(&sc);
 
-	char* pos = strstr(ausgabe, "1. Loch");
-	ASSERT_GE(pos, ausgabe);
+	ASSERT_TRUE(StrContains(ausgabe, "1. Loch"));
 }
