942673ae6d4e7d7df6195b876d5201f1f63f60d1
Alex Bepple <alex@bepple.de>
2014-05-19 15:44:27 +0200

    avoid invalid conversion

--- test/assertions.cc
+++ test/assertions.cc
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 
 ::testing::AssertionResult StrContains(const char* haystack, const char* needle) {
-	char* pos = strstr(haystack, needle);
+	const char* pos = strstr(haystack, needle);
 	if (pos > haystack) {
 		return ::testing::AssertionSuccess();
 	}
